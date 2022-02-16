69a2db87749c4c52ac57e64c908f9d694d2a3702
Abbos Donaboyev <donaboyevabbos1701@gmail.com>
2022-02-16 17:04:55 +0500

    tracking app bar visible or not is working fine

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import '../responsive.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      appBar: Responsive.isDesktop(context)
+          ? null
+          : AppBar(
+              backgroundColor: Colors.white,
+              elevation: 0,
+              toolbarHeight: 50,
+              actions: [
+                Builder(
+                  builder: (context) => Padding(
+                    padding: const EdgeInsets.only(right: 18.0),
+                    child: IconButton(
+                      icon: SvgPicture.asset('assets/images/svg/ic_menu.svg'),
+                      onPressed: () {
+                        debugPrint('======> open drawer');
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+      body: const Center(
         child: Text('Home screen'),
       ),
     );
