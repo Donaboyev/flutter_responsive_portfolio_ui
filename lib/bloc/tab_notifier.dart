c56634712f96e2edeca88e635dc1ccedc18d8474
Abbos Donaboyev <donaboyevabbos1701@gmail.com>
2022-03-16 16:38:36 +0500

    latest changes

--- lib/bloc/tab_notifier.dart
+++ lib/bloc/tab_notifier.dart
@@ -6,101 +6,36 @@ import '../screens/screens.dart';
 enum Screen { home, works, blog, contact }
 
 class TabNotifier extends ChangeNotifier {
-  Screen _screen = Screen.home;
-
-  Screen get screen => _screen;
-
   Widget _currentScreen = const HomeScreen();
 
   Widget get currentScreen => _currentScreen;
 
-  // void changeScreen(BuildContext context, Screen value) async {
-  //   switch (value) {
-  //     case Screen.home:
-  //       if (_screen == Screen.home) {
-  //         return;
-  //       }
-  //       context.router.popUntilRouteWithName(HomeRoute.name);
-  //       break;
-  //     case Screen.works:
-  //       if (_screen == Screen.works) {
-  //         return;
-  //       }
-  //       final result = await context.pushRoute(const WorksRoute());
-  //       if(result!=null) {
-  //         changeScreen(context, Screen.home);
-  //       }
-  //       break;
-  //     case Screen.blog:
-  //       final result = await context.pushRoute(const BlogRoute());
-  //       if(result!=null) {
-  //         changeScreen(context, Screen.home);
-  //       }
-  //       break;
-  //     case Screen.contact:
-  //       if (_screen == Screen.contact) {
-  //         return;
-  //       }
-  //       final result = await context.pushRoute(const ContactRoute());
-  //       debugPrint('=====> result : $result');
-  //       if (result != null) {
-  //         debugPrint('=====> result is not null');
-  //         changeScreen(context, Screen.home);
-  //       }
-  //       break;
-  //     default:
-  //       final result = await context.pushRoute(const HomeRoute());
-  //       debugPrint('=====> result : $result');
-  //       if (result != null) {
-  //         debugPrint('=====> result is not null');
-  //         changeScreen(context, Screen.home);
-  //       }
-  //       break;
-  //   }
-  //   _screen = value;
-  //   notifyListeners();
-  // }
+  Screen _screen =Screen.home;
 
+  Screen get screen => _screen;
 
-  void changeScreen(BuildContext context, Screen value) {
-    _screen = value;
-    switch (value) {
+  void updateCurrentScreen(BuildContext context, Screen screen) {
+    _screen = screen;
+    switch (screen) {
       case Screen.home:
         context.go('/home');
+        _currentScreen = const HomeScreen();
         break;
       case Screen.works:
         context.go('/works');
+        _currentScreen = const WorksScreen();
         break;
       case Screen.blog:
         context.go('/blog');
+        _currentScreen = const BlogScreen();
         break;
       case Screen.contact:
         context.go('/contact');
+        _currentScreen = const ContactScreen();
         break;
       default:
         context.go('/home');
-        break;
-    }
-    notifyListeners();
-  }
-
-  void updateCurrentScreen(BuildContext context,String screen) {
-    context.go(screen);
-    switch(screen) {
-      case '/home':
-        _currentScreen= const HomeScreen();
-        break;
-      case '/works':
-        _currentScreen= const WorksScreen();
-        break;
-      case '/blog':
-        _currentScreen= const BlogScreen();
-        break;
-      case '/contact':
-        _currentScreen= const ContactScreen();
-        break;
-      default:
-        _currentScreen= const HomeScreen();
+        _currentScreen = const HomeScreen();
         break;
     }
     notifyListeners();
--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 import 'package:responsive_portfolio_ui/core/app_assets.dart';
-import 'package:responsive_portfolio_ui/screens/home/home_screen.dart';
 
 import '../bloc/tab_notifier.dart';
 import '../components/components.dart';
@@ -17,147 +16,135 @@ class MainScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
     return Consumer<TabNotifier>(
-      builder: (context, tabNotifier, child) => WillPopScope(
-        onWillPop: () {
-          debugPrint('=======> back button clicked');
-          tabNotifier.changeScreen(context, Screen.home);
-          return Future<bool>.value(true);
-        },
-        child: Scaffold(
-          backgroundColor: clrWhite,
-          endDrawer: Responsive.isDesktop(context)
-              ? null
-              : Drawer(
-                  backgroundColor: clrWhite,
-                  child: Padding(
-                    padding: const EdgeInsets.all(18.0),
-                    child: Column(
-                      children: [
-                        const SizedBox(height: 40),
-                        Align(
-                          alignment: Alignment.centerLeft,
-                          child: IconButton(
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                            icon: const Icon(Icons.close),
-                          ),
-                        ),
-                        const SizedBox(height: 40),
-                        CustomTextButton(
-                          isSelected: tabNotifier.screen == Screen.home,
-                          text: Strings.home,
+      builder: (context, tabNotifier, child) => Scaffold(
+        backgroundColor: clrWhite,
+        endDrawer: Responsive.isDesktop(context)
+            ? null
+            : Drawer(
+                backgroundColor: clrWhite,
+                child: Padding(
+                  padding: const EdgeInsets.all(18.0),
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 40),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: IconButton(
                           onPressed: () {
-                            tabNotifier.changeScreen(context, Screen.home);
                             Navigator.pop(context);
                           },
+                          icon: const Icon(Icons.close),
                         ),
-                        const SizedBox(height: 20),
-                        CustomTextButton(
-                          isSelected: tabNotifier.screen == Screen.works,
-                          text: Strings.works,
-                          onPressed: () {
-                            tabNotifier.changeScreen(context, Screen.works);
-                            Navigator.pop(context);
-                          },
-                        ),
-                        const SizedBox(height: 20),
-                        CustomTextButton(
-                          isSelected: tabNotifier.screen == Screen.blog,
-                          text: Strings.blog,
-                          onPressed: () {
-                            tabNotifier.changeScreen(context, Screen.blog);
-                            Navigator.pop(context);
-                          },
-                        ),
-                        const SizedBox(height: 20),
-                        CustomTextButton(
-                          isSelected: tabNotifier.screen == Screen.contact,
-                          text: Strings.contact,
-                          onPressed: () {
-                            tabNotifier.changeScreen(context, Screen.contact);
-                            Navigator.pop(context);
-                          },
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-          appBar: Responsive.isDesktop(context)
-              ? AppBar(
-                  toolbarHeight: 70,
-                  backgroundColor: clrWhite,
-                  elevation: 0,
-                  titleSpacing: 60 +
-                      ((size.width > maxWidth)
-                          ? (size.width - maxWidth) / 2
-                          : 0),
-                  title: CustomTextButton(
-                    text: Strings.home,
-                    onPressed: () {
-                      tabNotifier.changeScreen(context, Screen.home);
-                    },
-                    isSelected: tabNotifier.screen == Screen.home,
-                  ),
-                  actions: [
-                    CustomTextButton(
-                      isSelected: tabNotifier.screen == Screen.works,
-                      text: Strings.works,
-                      onPressed: () {
-                        tabNotifier.changeScreen(context, Screen.works);
-                      },
-                    ),
-                    const SizedBox(width: 33),
-                    CustomTextButton(
-                      isSelected: tabNotifier.screen == Screen.blog,
-                      text: Strings.blog,
-                      onPressed: () {
-                        tabNotifier.changeScreen(context, Screen.blog);
-                      },
-                    ),
-                    const SizedBox(width: 33),
-                    CustomTextButton(
-                      isSelected: tabNotifier.screen == Screen.contact,
-                      text: Strings.contact,
-                      onPressed: () {
-                        tabNotifier.changeScreen(context, Screen.contact);
-                      },
-                    ),
-                    SizedBox(
-                      width: 60 +
-                          ((size.width > maxWidth)
-                              ? (size.width - maxWidth) / 2
-                              : 0),
-                    ),
-                  ],
-                )
-              : AppBar(
-                  toolbarHeight: 50,
-                  backgroundColor: clrWhite,
-                  elevation: 0,
-                  actions: [
-                    Builder(
-                      builder: (context) => IconButton(
-                        onPressed: () {
-                          Scaffold.of(context).openEndDrawer();
-                        },
-                        icon: SvgPicture.asset(AppAssets.icMenu),
                       ),
-                    ),
-                    const SizedBox(width: 18),
-                  ],
+                      const SizedBox(height: 40),
+                      CustomTextButton(
+                        isSelected: tabNotifier.screen == Screen.home,
+                        text: Strings.home,
+                        onPressed: () {
+                          tabNotifier.updateCurrentScreen(
+                              context, Screen.home);
+                        },
+                      ),
+                      const SizedBox(height: 20),
+                      CustomTextButton(
+                        isSelected: tabNotifier.screen == Screen.works,
+                        text: Strings.works,
+                        onPressed: () {
+                          tabNotifier.updateCurrentScreen(
+                              context, Screen.works);
+                        },
+                      ),
+                      const SizedBox(height: 20),
+                      CustomTextButton(
+                        isSelected: tabNotifier.screen == Screen.blog,
+                        text: Strings.blog,
+                        onPressed: () {
+                          tabNotifier.updateCurrentScreen(
+                              context, Screen.blog);
+                        },
+                      ),
+                      const SizedBox(height: 20),
+                      CustomTextButton(
+                        isSelected: tabNotifier.screen == Screen.contact,
+                        text: Strings.contact,
+                        onPressed: () {
+                          tabNotifier.updateCurrentScreen(
+                              context, Screen.contact);
+                        },
+                      ),
+                    ],
+                  ),
                 ),
-          body: Container(
-            constraints: const BoxConstraints(maxWidth: maxWidth),
-            child: WillPopScope(
-              onWillPop: () {
-                debugPrint('=====> mana back button');
-                return Future<bool>.value(true);
-              },
-              child: Expanded(
-                child: tabNotifier.currentScreen,
               ),
-            ),
+        appBar: Responsive.isDesktop(context)
+            ? AppBar(
+                toolbarHeight: 70,
+                backgroundColor: clrWhite,
+                elevation: 0,
+                titleSpacing: 60 +
+                    ((size.width > maxWidth)
+                        ? (size.width - maxWidth) / 2
+                        : 0),
+                title: CustomTextButton(
+                  text: Strings.home,
+                  onPressed: () {
+                    tabNotifier.updateCurrentScreen(context, Screen.home);
+                  },
+                  isSelected: tabNotifier.screen == Screen.home,
+                ),
+                actions: [
+                  CustomTextButton(
+                    isSelected: tabNotifier.screen == Screen.works,
+                    text: Strings.works,
+                    onPressed: () {
+                      tabNotifier.updateCurrentScreen(context, Screen.works);
+                    },
+                  ),
+                  const SizedBox(width: 33),
+                  CustomTextButton(
+                    isSelected: tabNotifier.screen == Screen.blog,
+                    text: Strings.blog,
+                    onPressed: () {
+                      tabNotifier.updateCurrentScreen(context, Screen.blog);
+                    },
+                  ),
+                  const SizedBox(width: 33),
+                  CustomTextButton(
+                    isSelected: tabNotifier.screen == Screen.contact,
+                    text: Strings.contact,
+                    onPressed: () {
+                      tabNotifier.updateCurrentScreen(
+                          context, Screen.contact);
+                    },
+                  ),
+                  SizedBox(
+                    width: 60 +
+                        ((size.width > maxWidth)
+                            ? (size.width - maxWidth) / 2
+                            : 0),
+                  ),
+                ],
+              )
+            : AppBar(
+                toolbarHeight: 50,
+                backgroundColor: clrWhite,
+                elevation: 0,
+                actions: [
+                  Builder(
+                    builder: (context) => IconButton(
+                      onPressed: () {
+                        Scaffold.of(context).openEndDrawer();
+                      },
+                      icon: SvgPicture.asset(AppAssets.icMenu),
+                    ),
+                  ),
+                  const SizedBox(width: 18),
+                ],
+              ),
+        body: Center(
+          child: Container(
+            constraints: const BoxConstraints(maxWidth: maxWidth),
+            child: tabNotifier.currentScreen,
           ),
         ),
       ),
